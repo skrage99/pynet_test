960d5ae8fc6f687f4158987511c52bc07a777357
Shaun K <krage.shaun@mayo.edu>
2019-02-05 10:04:01 -0800

    string 1 exercise

---
+++ day1/str_ex1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+name1 = "Shaun"
+name2 = "Sienna"
+name3 = "Stacy" 
+name4 = input("Enter fourth name: ")
+
+print("{:>30}".format(name1))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+print("{:>30}".format(name4))
+
+
+
+
